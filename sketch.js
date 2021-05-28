9bd52f6222ffe50c6512f51f4ab937075c5eb0e7
omkarkarale <82709945+omkarkarale@users.noreply.github.com>
2021-05-29 00:00:37 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,32 @@
-var laser3 = createSprite(5, 200, 10, 5);
-laser3.shapeColor = "red";
-laser3.velocityX = 35;
-var laser1 = createSprite(300, 378, 200, 5);
-laser1.shapeColor = "red";
-laser1.velocityY = -2;
-var laser2 = createSprite(100, 25, 200, 5);
-laser2.shapeColor = "red";
-laser2.velocityY = 2;
-var thief = createSprite(10, 390, 20, 20);
-var diabox = createSprite(390, 10, 25, 25);
-diabox.shapeColor = "blue";
+var laser1,laser2,laser3,thief,diabox,edges;
+
+function setup(){
+  laser3 = createSprite(5, 200, 10, 5);
+  laser3.shapeColor = "red";
+  laser3.velocityX = 35;
+  laser1 = createSprite(300, 378, 200, 5);
+  laser1.shapeColor = "red";
+  laser1.velocityY = -2;
+  laser2 = createSprite(100, 25, 200, 5);
+  laser2.shapeColor = "red";
+  laser2.velocityY = 2;
+  thief = createSprite(10, 390, 20, 20);
+  diabox = createSprite(390, 10, 25, 25);
+  diabox.shapeColor = "blue";
+  edges = new Group();
+  up_edge = createSprite(width/2,0,width,1);
+  // up_edge.visible = false;
+  edges.add(up_edge);
+  right_edge = createSprite(width,height/2,1,height);
+  // right_edge.visible = false;
+  edges.add(right_edge);
+  bottom_edge = createSprite(width/2,height,width,1);
+  // bottom_edge.visible = false;
+  edges.add(bottom_edge);
+  left_edge = createSprite(0,height/2,1,height);
+  // left_edge.visible = false;
+  edges.add(left_edge);
+}
 function draw() {
   background("black");
   shape(390, 0, 380, 10, 390, 20, 400, 10);
